0324d63b83799307b02ea7f8a138af8abf09ee0d
lolzxc <guitarbeginner2@gmail.com>
2022-05-03 21:11:35 +0800

    Initial Commit

---
+++ app/src/main/java/com/example/todomvvm/data/TodoRepository.kt
@@ -0,0 +1,19 @@
+package com.example.todomvvm.data
+
+import kotlinx.coroutines.flow.Flow
+
+interface TodoRepository {
+
+
+    suspend fun insertTodo(todo: Todo)
+
+
+    suspend fun deleteTodo(todo: Todo)
+
+
+    suspend fun getTodoById(id: Int): Todo?
+
+
+    fun getTodos(): Flow<List<Todo>>
+
+}
