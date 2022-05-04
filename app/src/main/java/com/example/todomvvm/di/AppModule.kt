937187cd2270195f7e3a55af61fa90bbe931e65f
lolzxc <guitarbeginner2@gmail.com>
2022-05-04 16:52:28 +0800

    Initial Commit

--- app/src/main/java/com/example/todomvvm/di/AppModule.kt
+++ app/src/main/java/com/example/todomvvm/di/AppModule.kt
@@ -1,12 +1,36 @@
 package com.example.todomvvm.di
 
+import android.app.Application
+import androidx.room.Room
+import com.example.todomvvm.data.TodoDatabase
+import com.example.todomvvm.data.TodoRepository
+import com.example.todomvvm.data.TodoRepositoryImplementation
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 
 object AppModule {
 
+    @Provides
+    @Singleton
+
+    fun provideTodoDatabase(app: Application): TodoDatabase{
+        return Room.databaseBuilder(
+            app,
+            TodoDatabase::class.java,
+            "todo_db"
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+
+    fun provideTodoRepository(db: TodoDatabase): TodoRepository{
+        return TodoRepositoryImplementation(db.dao)
+    }
 }
---
+++ app/src/main/java/com/example/todomvvm/ui/todo_list/TodoListEvent.kt
@@ -0,0 +1,13 @@
+package com.example.todomvvm.ui.todo_list
+
+import com.example.todomvvm.data.Todo
+
+sealed class TodoListEvent{
+    data class OnDeleteTodoClick(val todo: Todo): TodoListEvent()
+    data class OnDoneChange(val todo: Todo, val isDone: Boolean): TodoListEvent()
+    object OnUndoDeleteClick: TodoListEvent()
+    data class OnTodoClick(val todo: Todo): TodoListEvent()
+    object OnAddTodoClick: TodoListEvent()
+
+
+}
